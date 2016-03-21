9cdbc53b111bb3dc98db00ba7f8fe6ab58fd6358
endless3cross3 <endless3cross3@gmail.com>
2016-03-21 18:40:47 +0800

    first commit

---
+++ data/pcv1.1_01.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from PIL import Image
+from pylab import *
+
+# 添加中文字體支援
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname=r"c:\windows\fonts\msjh.ttf", size=14)
+figure()
+
+pil_im = Image.open('../data/empire.jpg')
+gray()
+subplot(121)
+title(u'原圖',fontproperties=font)
+axis('off')
+imshow(pil_im)
+
+pil_im = Image.open('../data/empire.jpg').convert('L')
+subplot(122)
+title(u'灰階圖',fontproperties=font)
+axis('off')
+imshow(pil_im)
+
+show()
